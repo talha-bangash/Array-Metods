ce994f343bff8ddfc6224241c2d49224c7c47b02
talha-bangash <tmbangash2006@gmail.com>
2024-07-24 21:08:15 +0500

    done-it

---
+++ script.js
@@ -0,0 +1,16 @@
+let names = ["Talha", "Ali", "Khan", "Ahmed", "Shahid", "Anwar"];
+
+
+
+names.pop()
+
+names.push("akeel");
+
+names.shift();
+
+names.unshift("waleed", "Moiz");
+
+names.splice(2, 3, "Hafeez", "Imran");
+
+names2 = names.slice(1,4);
+console.log(names2);
